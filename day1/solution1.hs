d1e78398be280b86e35819d8bc70629acbbec026
Peter Caisse <pcaisse@gmail.com>
2021-12-02 00:09:59 -0500

    Finish day 1

--- day1/solution1.hs
+++ day1/solution1.hs
@@ -7,8 +7,13 @@ countIncreasing [x, y] = if y > x then 1 else 0
 countIncreasing (x : y : rest) = countIncreasing [x, y] + countIncreasing (y : rest)
 countIncreasing _ = 0
 
+sumEvery3 :: [Int] -> [Int]
+sumEvery3 [x, y, z] = [x + y + z]
+sumEvery3 (x : y : z : rest) = sumEvery3 [x, y, z] ++ sumEvery3 (y : z : rest)
+sumEvery3 l = l
+
 main :: IO ()
 main = do
     input <- getContents
     let measurements = map (\a -> read a :: Int) (lines input)
-    putStr $ show $ countIncreasing measurements
+    putStr $ show $ sumEvery3 measurements
