28ec8cdbee639f06b14d7d25d1b645e54c07b552
Peter Caisse <pcaisse@gmail.com>
2021-12-03 20:16:57 -0500

    Second part of day 3

--- day3/solution.hs
+++ day3/solution.hs
@@ -6,26 +6,31 @@ parseBinary x = case readBin x of
     [(int,_)] -> int
     _ -> error "bad input"
 
-bits :: Int -> [Int] -> [Int]
-bits n ints = map (\x -> if x .&. bit (n - 1) /= 0 then 1 else 0) ints
+bitAtIndex :: Int -> Int -> Int -> Int
+bitAtIndex numDigits index int = if int .&. bit (numDigits - 1 - index) /= 0 then 1 else 0
 
-bitListToDec :: [Int] -> Int
-bitListToDec bits =
-    let zippedBits = zip [0..] (reverse bits)
-    in foldr sumHelper 0 zippedBits
-    where
-       sumHelper (i, value) acc = acc + value * 2 ^ i
+-- return all bits for one column in matrix by offset (0 is first bit)
+bitsByColumnIndex :: Int -> Int -> [Int] -> [Int]
+bitsByColumnIndex numDigits offset ints = map (\x -> bitAtIndex numDigits offset x) ints
+
+findRating :: Int -> Int -> Bool -> [Int] -> Int
+findRating numDigits index most [] = error "something went wrong"
+findRating numDigits index most [int] = int
+findRating numDigits index most ints =
+    let bits = bitsByColumnIndex numDigits index ints
+        ones = filter (== 1) bits
+        zeroes = filter (== 0) bits
+        mostCommonValue = if length ones >= length zeroes then 1 else 0
+        leastCommonValue = if length ones >= length zeroes then 0 else 1
+        value = if most then mostCommonValue else leastCommonValue
+        intsWithCommonValue = filter (\int -> bitAtIndex numDigits index int == value) ints
+    in findRating numDigits (index + 1) most intsWithCommonValue
 
 main :: IO ()
 main = do
     input <- getContents
-    let allLines = lines input
-    let numDigits = length $ head allLines
-    let numLines = length allLines
-    let ints = map parseBinary allLines
-    let columnBits = [bits i ints | i <- reverse [1..numDigits]]
-    let columnOnes = map (filter (== 1)) columnBits
-    let bitList = map (\l -> if length l > numLines `div` 2 then 1 else 0) columnOnes
-    let gamma = bitListToDec bitList
-    let epsilon = bitListToDec (map (xor 1) bitList)
-    putStr $ show $ gamma * epsilon
+    let ints = map parseBinary (lines input)
+    let numDigits :: Int = ceiling $ logBase 2 $ fromIntegral $ maximum ints
+    let oxygen = findRating numDigits 0 True ints
+    let co2 = findRating numDigits 0 False ints
+    putStr $ show $ oxygen * co2
