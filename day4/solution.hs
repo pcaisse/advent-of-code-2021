3e401433d2f1e7019827aaafa91bc1cccd2f5adc
Peter Caisse <pcaisse@gmail.com>
2021-12-05 13:31:44 -0500

    Parse input

--- day4/solution.hs
+++ day4/solution.hs
@@ -1,11 +1,32 @@
-module Main where
-
 import Text.Parsec
+import Text.Parsec.Text (Parser)
+import Text.Parsec.Char (newline)
+import Data.Text (pack)
+
+nat :: Parser Int
+nat = read `fmap` many1 digit
+
+draws :: Parser [Int]
+draws = nat `sepBy` char ','
+
+cardRow :: Parser [Int]
+cardRow = count 5 (spaces *> nat <* spaces)
+
+card :: Parser [[Int]]
+card = count 5 cardRow
+
+cards :: Parser [[[Int]]]
+cards = many card
+
+drawsAndCards :: Parser ([Int], [[[Int]]])
+drawsAndCards = do
+    draws <- draws
+    count 2 newline
+    cards <- cards <* eof
+    return (draws, cards)
 
 main :: IO ()
 main = do
     input <- getContents
-    let linesWithContent = filter (/= "") (lines input)
-    let draws = head linesWithContent
-    let cards = tail linesWithContent
-    putStr $ show draws ++ show cards
+    let result = parse drawsAndCards "(source)" (pack input)
+    putStr $ show result
