90fcc501dd27f3d77441f8ad1947fa612a730948
Peter Caisse <pcaisse@gmail.com>
2021-12-06 00:34:19 -0500

    Finish part 1 of day 4

--- day4/solution.hs
+++ day4/solution.hs
@@ -2,6 +2,8 @@ import Text.Parsec
 import Text.Parsec.Text (Parser)
 import Text.Parsec.Char (newline)
 import Data.Text (pack)
+import Data.List
+import Data.Maybe
 
 nat :: Parser Int
 nat = read `fmap` many1 digit
@@ -25,8 +27,28 @@ drawsAndCards = do
     cards <- cards <* eof
     return (draws, cards)
 
+cardWins :: [Int] -> [[Int]] -> Bool
+cardWins draws card = any (\series -> length (draws `intersect` series) == 5) (card ++ transpose card)
+
+scoreCard :: [Int] -> [[Int]] -> Int
+scoreCard draws card =
+    let unmarkedNumbers = filter (`notElem` draws) (concat card)
+        lastNumberCalled = last draws
+    in sum unmarkedNumbers * lastNumberCalled
+
+checkWin :: [Int] -> Int -> [[[Int]]] -> Int
+checkWin draws n cards =
+    if n > length draws then error "no winning cards" else
+    case catMaybes [check card (take n draws) | card <- cards] of
+        [] -> checkWin draws (n + 1) cards
+        (score : _) -> score
+    where
+        check card currentDraws =
+            if cardWins currentDraws card then Just (scoreCard currentDraws card) else Nothing
+
 main :: IO ()
 main = do
     input <- getContents
-    let result = parse drawsAndCards "(source)" (pack input)
-    putStr $ show result
+    case parse drawsAndCards "(source)" (pack input) of
+        Left error -> putStr $ show error
+        Right (draws, cards) -> putStr $ show $ checkWin draws 5 cards
