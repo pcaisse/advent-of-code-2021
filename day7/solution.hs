5d504ebeb68d3860454c030230f823d486134f8a
Peter Caisse <pcaisse@gmail.com>
2021-12-07 15:08:32 -0500

    First part of day 7

--- day7/solution.hs
+++ day7/solution.hs
@@ -7,10 +7,19 @@ import Data.List
 crabs :: Parser [Int]
 crabs = fmap read (many digit) `sepBy` char ','
 
+median :: [Int] -> Int
+median sortedLs = sortedLs !! (length sortedLs `div` 2)
+
+fuel :: [Int] -> Int
+fuel crabs =
+    let sortedCrabs = sort crabs
+        med = median sortedCrabs
+    in sum $ map (\pos -> abs $ pos - med) sortedCrabs
+
 main :: IO ()
 main = do
     input <- getContents
     case parse (crabs <* newline) "(source)" (pack input) of
         Left error -> putStr $ show error
-        Right crabs -> putStr $ show crabs
+        Right crabs -> putStr $ show $ fuel crabs
 
